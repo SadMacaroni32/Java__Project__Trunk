81f23e0867f316526500b7e6a0cd710635953433
JE <flash.staffsuport@gmail.com>
2024-10-08 10:33:45 +0800

    Final

--- UDP-Socket-Programming-JAVA/UDPClient.java
+++ UDP-Socket-Programming-JAVA/UDPClient.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 
 public class UDPClient {
     public static void main(String[] args) throws IOException {
@@ -10,9 +11,15 @@ public class UDPClient {
         byte[] buffer = message.getBytes();
         InetAddress address = InetAddress.getByName("localhost");
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 3000);
+        socket.setSoTimeout(5000);
+        try {
         socket.send(packet);
         socket.receive(packet);
         System.out.println("Recieved: " + new String(packet.getData()));
         socket.close();
+        } catch (SocketTimeoutException e){
+            System.out.println("Recieve Timed Out");
+        }
+        
     }
 }
--- UDP-Socket-Programming-JAVA/UDPServer.java
+++ UDP-Socket-Programming-JAVA/UDPServer.java
@@ -1,18 +1,23 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketTimeoutException;
 
 public class UDPServer {
     public static void main(String[] args) throws IOException {
         DatagramSocket socket = new DatagramSocket(3000);
         byte[] buffer = new byte[1024];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        
         while(true) {
+            socket.setSoTimeout(5000);
+            try{ 
             socket.receive(packet);
             System.out.println("Recieved: " + new String(packet.getData()));
             socket.send(packet);
+            } catch (SocketTimeoutException e) {
+                System.out.println("Recieved Time Out. Awaiting Packet.");
+            }
         }
-
-        
     }
 }
